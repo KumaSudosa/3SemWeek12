b0a3421107573ac5dc4429af2b2ef5b382c3fa2a
Marcus Johnsen <cph-mj734@cphbusiness.dk>
2020-03-27 13:48:41 +0100

    ListsAndKeys assignment

---
+++ Wednesday/lists-and-keys/src/ListDemoApp2.js
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+
+function MemberTable({ members }) {
+  return (
+    <table>
+      <thead>
+        <th>Name</th>
+        <th>Age</th>
+      </thead>
+      <tbody>
+        {members.map(m => {
+          return (
+            <tr>
+              <td>{m.name}</td>
+              <td>{m.age}</td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+}
+
+function MemberDemo(props) {
+  return (
+    <div>
+      <h4>All Members</h4>
+      <MemberTable members={props.members} />
+    </div>
+  );
+}
+
+export default function App() {
+  const initialMembers = [
+    { name: "Peter", age: 18 },
+    { name: "Jan", age: 35 },
+    { name: "Janne", age: 25 },
+    { name: "Martin", age: 22 }
+  ];
+  const [members] = useState(initialMembers);
+
+  return <MemberDemo members={members} />;
+}
